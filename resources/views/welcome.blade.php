118f08c07bd9c7a83d79e533d1c08e4e9cff2ff1
fatandaz <fatandaz@gmail.com>
2020-09-10 22:43:06 +0200

    template de buscar - disponible para descargar como proyecto base en laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,20 @@
 
 @section('content')
     <div class="container">
+        <div class="container-fluid imagen_blockchain text-white">
 
+            <div class="align-content-center size">
+                <h2>Buscar</h2>
+                <!--p>Ingrese un address para encontrar las transacciones en la red de pruebas testnet3</p-->
+                <form class="form-inline" action="/api/addressFull">
+                    <label for="address" class="mb-2 mr-sm-2">Bitcoin Address:</label>
+                    <input type="text" class="form-control mb-2 mr-sm-2 input-group-lg" id="search_address"
+                           placeholder="n3AmuXTmVtPRZfm1zqZG5bVFR4QGxZM2RE" value="n3AmuXTmVtPRZfm1zqZG5bVFR4QGxZM2RE"
+                           name="address">
+                    <button type="submit" title="Search" class="btn btn-primary mb-2"> Buscar</button>
+                </form>
+            </div>
 
-        <div class="container-fluid imagen_blockchain">
-
-          Freddy
         </div>
     </div>
 @endsection
